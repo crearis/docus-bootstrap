a16523de72932e4f280985e31bb2cfe1ca15a664
crearis <hans.doenitz@dasei.eu>
2023-04-26 11:56:54 +0200

    added coreui

--- pages/bootstrap.vue
+++ pages/bootstrap.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+  import { CAlert } from '@coreui/vue'
+  import '@coreui/coreui/dist/css/coreui.min.css'
+</script>
+
 <template>
-  <h1>Bootstrap Test page</h1>
+  <div>
+    <h1>Bootstrap Test page</h1>
+    <CAlert color="primary">
+      A simple primary alert—check it out!
+    </CAlert>
+  </div>
 </template>
+
